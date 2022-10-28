b836dd6669e9bbec989299811ada8b04402bd319
Abel Mendonça <abel.ncm@gmail.com>
2022-10-28 14:50:28 -0100

    listCalculation

--- js/main.js
+++ js/main.js
@@ -4,9 +4,7 @@ console.log('exercise')
 
 // console.log(myList)
 
-
 function multiplyArrayItemsBy(myList, multiplyNumber) {
-
     for(pos in myList) {
         myList[pos] = myList[pos] * multiplyNumber;
     }
@@ -15,3 +13,50 @@ function multiplyArrayItemsBy(myList, multiplyNumber) {
 }
 
 // multiplyArrayItemsBy([2,1,4], 2)
+
+function sayHello(name) {
+    // let greetingText = 'Hello! How are you today ' + name + '?';
+    let greetingText = `Hello! How are you today ${name}?`;
+    return greetingText;
+}
+
+function addToArrayItems(myList, appendValue) {
+
+    let total = 0;
+
+    for(pos in myList) {
+        total += value
+        myList[pos] = myList[pos] + appendValue;
+    }
+
+    return myList;
+}
+
+function listCalculation(myList, operator) {
+    let total = myList[0];
+    
+    if(operator == '+') {
+        for(var i=1; i<myList.length; i++) {
+            total += myList[i];
+        }
+
+    } else if (operator == '-') {
+        for(var i=1; i<myList.length; i++) {
+            total -= myList[i];
+        }
+    } else if (operator == '*') {
+        for(var i=1; i<myList.length; i++) {
+            total *= myList[i];
+        }
+    } else if (operator == '/') {
+        for(var i=1; i<myList.length; i++) {
+            total /= myList[i];
+        }
+    }
+
+    return total;
+
+}
+
+
+['How', 'Are you today', 'Hi']
