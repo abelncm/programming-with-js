390bd3a9ec917838307addc7f2bfc042a050de4e
belarmino <belarmino.veiga@gmail.com>
2022-11-04 14:34:14 -0100

    data

--- js/exercises/datetime.js
+++ js/exercises/datetime.js
@@ -13,3 +13,30 @@ function addTime(date,hour){
     let y = new Date(date.setUTCHours(date.getHours()+1+hour))
     return y;
 }
+
+function is_week_day(date){
+    if(date.getDay()==0 || date.getDay()==6){
+        return "Fim de semana"
+    }else{
+        return "Dia de semana";
+    }
+}
+
+function yesterday(date){
+    date = date.setDate(date.getDate()-1)
+    return new Date(date); 
+}
+
+function min_date(dates){
+    let minDate = undefined;
+    for(let i=0;i<dates.length;i++){
+        if(i==0){
+            minDate = dates[i];
+        }else{
+            if(minDate > dates[i]){
+                minDate=dates[i];
+            }
+        }        
+    }
+    return minDate; 
+}
