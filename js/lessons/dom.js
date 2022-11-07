bac256d4f62565421cac6bb89f27092d3b1e4d54
Abel Mendonça <abel.ncm@gmail.com>
2022-11-07 14:59:00 -0100

    lisa work

--- js/lessons/dom.js
+++ js/lessons/dom.js
@@ -4,7 +4,7 @@ function concatAlert() {
     let text = '';
 
     myPList.forEach(el => {
-        text += el.textContent + '\n' ;
+        text += el.textContent + '\n';
     })
 
     alert(text);
@@ -22,6 +22,42 @@ function replaceItem4() {
     })
 }
 
-function counter() {
+// function increment() {
+//     let span = document.querySelector('#counter span');
+    
+//     let number = parseInt(span.textContent);
+//     let total = number + 1;
 
+//     span.textContent = total;
+// }
+
+function colorama(span){
+    if (span.textContent<=5){
+        span.style.color='blue'
+    }else {
+        span.style.color='green'
+    }
+}
+
+function increment() {
+    let span = document.querySelector('#counter span');
+    if (span.textContent<10){
+        span.textContent = parseInt(span.textContent)+1;
+        colorama(span)
+    }
+    
+}
+
+function decrement(){
+    let span=document.querySelector('#counter span');
+    if (span.textContent>=2) {
+        span.textContent=parseInt(span.textContent)-1;
+        colorama(span)        
+    }
+        
+}
+
+function reset(){
+    let span=document.querySelector('#counter span');
+    span.textContent=1
 }
