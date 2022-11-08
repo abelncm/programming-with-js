59efbf243667cd65f967aa79ac35efcf8d045f34
Abel Mendonça <abel.ncm@gmail.com>
2022-11-08 15:23:00 -0100

    totoloto

--- js/lessons/dom.js
+++ js/lessons/dom.js
@@ -24,66 +24,66 @@ function replaceItem4() {
 
 // function increment() {
 //     let span = document.querySelector('#counter span');
-    
+
 //     let number = parseInt(span.textContent);
 //     let total = number + 1;
 
 //     span.textContent = total;
 // }
 
-function colorama(span){
-    if (span.textContent<=5){
-        span.style.color='blue'
-    }else {
-        span.style.color='green'
+function colorama(span) {
+    if (span.textContent <= 5) {
+        span.style.color = 'blue'
+    } else {
+        span.style.color = 'green'
     }
 }
 
 function increment() {
     let span = document.querySelector('#counter span');
-    if (span.textContent<10){
-        span.textContent = parseInt(span.textContent)+1;
+    if (span.textContent < 10) {
+        span.textContent = parseInt(span.textContent) + 1;
         colorama(span)
     }
-    
+
 }
 
-function decrement(){
-    let span=document.querySelector('#counter span');
-    if (span.textContent>=2) {
-        span.textContent=parseInt(span.textContent)-1;
-        colorama(span)        
+function decrement() {
+    let span = document.querySelector('#counter span');
+    if (span.textContent >= 2) {
+        span.textContent = parseInt(span.textContent) - 1;
+        colorama(span)
     }
-        
+
 }
 
-function reset(){
-    let span=document.querySelector('#counter span');
-    span.textContent=1
+function reset() {
+    let span = document.querySelector('#counter span');
+    span.textContent = 1
 }
 
-function changeBoxColor(){
+function changeBoxColor() {
     let selectedColor = document.querySelector('#colors').value;
     document.querySelector('.coloring-box').style.backgroundColor = selectedColor;
-    
+
 }
 
 
 let myInterval;
-function clockCounter(){
+function clockCounter() {
     document.querySelector('#clock-counter .start-btn')
         .setAttribute('disabled', 'true');
-    
-    myInterval = setInterval(function() {
+
+    myInterval = setInterval(function () {
         console.log('a executar');
 
         let span = document.querySelector('#clock-counter span');
         let num = parseInt(span.textContent);
-        
-        if(num==10)
+
+        if (num == 10)
             clearInterval(myInterval);
         else
-            span.textContent=num+1;
+            span.textContent = num + 1;
 
     }, 1000);
 }
@@ -98,19 +98,66 @@ function calculate() {
     let operator = document.querySelector('#calculator .operator').value;
     let span = document.querySelector('#calculator .result');
 
-    let result=0;
+    let result = 0;
 
-    if(operator == '+') {
+    if (operator == '+') {
         result = param1 + param2;
-    } else if(operator == '-') {
+    } else if (operator == '-') {
         result = param1 - param2;
-    }else if(operator == '/') {
+    } else if (operator == '/') {
         result = param1 / param2;
-    }else if(operator == '*') {
+    } else if (operator == '*') {
         result = param1 * param2;
     }
 
     span.textContent = result;
 }
 
-   
+function totolotoResult() {
+
+    let totolotoNumbers = [];
+
+    for (let i = 0; i < 6; i++) {
+        let generatedNumber = Math.floor(Math.random() * 49) + 1;
+        
+        while(totolotoNumbers.includes(generatedNumber)==true) {
+            generatedNumber = Math.floor(Math.random() * 49) + 1;
+        }
+
+        totolotoNumbers.push(generatedNumber);
+    }
+
+    console.log(totolotoNumbers);
+
+
+    let inputList = document.querySelectorAll("#totoloto input");
+
+    let correctNumbers = [];
+    let wrongNumbers = [];
+
+    for (input of inputList) {
+        console.log(input);
+
+        let userNumber = parseInt(input.value);
+
+        if (totolotoNumbers.includes(userNumber) == true) {
+            correctNumbers.push(userNumber);
+        } else {
+            wrongNumbers.push(userNumber);
+        }
+    }
+
+    document.querySelector("#totoloto .totoloto-num")
+        .textContent = sortArray(totolotoNumbers).join(' - ');
+
+    document.querySelector("#totoloto .correct-num")
+        .textContent = sortArray(correctNumbers).join(' - ');
+
+    document.querySelector("#totoloto .wrong-num")
+        .textContent = sortArray(wrongNumbers).join(' - ');
+}
+
+function sortArray(list) {
+    return list.sort(function (a, b) { return a - b })
+}
+
